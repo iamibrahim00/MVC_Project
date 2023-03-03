a974f005c57b467d7cff2b42ca282774e6ea288f
iamibrahim00 <iyamibrahimshaikh@gmail.com>
2023-03-03 19:18:16 +0530

    Adding product in input tag with body parser and logging it in the console

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const http = require('http');
 const express = require('express');
+const bodyparser = require('body-parser');
+
 
 const app = express();
 
-app.use((req,res,next)=>{
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.use('/add-product',(req,res,next)=>{
     console.log("In the Middleware")
-    next();
+    res.send('<form action ="/product" method="POST"><input type="text" name="title"> <input type="text" name="Size"><button type="submit">Add</button></form>')
 });
 
-app.use((req,res,next)=>{
-    console.log("In the next Middleware")
+app.use("/product",(req,res)=>{
+    console.log(req.body)
+    res.redirect('/') 
+})
+app.use('/',(req,res)=>{
+    //console.log("In the next Middleware") 
     res.send('<h1>Hello From Express</h1>')
 });
 
