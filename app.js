b2d891df879e4fa799a7652a9f7c2096fb31c0b8
iamibrahim00 <iyamibrahimshaikh@gmail.com>
2023-03-03 12:42:01 +0530

    Express Start Task(1) Middleware and req.send

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log("In the Middleware")
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log("In the next Middleware")
+    res.send('<h1>Hello From Express</h1>')
+});
+
+app.listen(3000)
