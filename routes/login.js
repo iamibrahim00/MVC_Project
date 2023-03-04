3590be3f6057b80efeb040d185f939009caedfb4
iamibrahim00 <iyamibrahimshaikh@gmail.com>
2023-03-04 19:56:04 +0530

    SimpleChatApp

---
+++ routes/login.js
@@ -0,0 +1,17 @@
+const express =require('express')
+const router = express.Router()
+
+router.get('/login',(req,res)=>{
+    res.send(` <form action="/login" method="POST" onsubmit="localStorage.setItem('username', document.getElementById('username').value)" >
+     <input id="username" type="text" name="username">
+     <button type="submit">Login</button>
+     </form>`)
+
+})
+
+router.post('/login',(req,res)=>{
+    //console.log(req.body)
+    res.redirect('/') 
+})
+
+module.exports = router
